e1ae0117ea7e363f2996e862e7d1427ffe9da4ed
Andrii.Mota <andrii_mota@ext.icicipruamc.com>
2022-12-06 16:18:49 +0200

    Initial commit

---
+++ car-selection/src/main/java/org/mota/carselectionservice/CarSelectionServiceApplication.java
@@ -0,0 +1,13 @@
+package org.mota.carselectionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSelectionServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CarSelectionServiceApplication.class);
+  }
+
+}
